d2db6d465f3a88114307606eb6c2af6c6fdb5576
Jose Romario Machado <machadodev03@gmail.com>
2023-05-30 19:50:39 -0300

    Ajustes e novas funções

--- src/FormHelper.php
+++ src/FormHelper.php
@@ -6,6 +6,12 @@ namespace FormHelper;
 class FormHelper
 {
     /**
+     * ####################
+     * ###   STRINGS   ###
+     * ####################
+     */
+
+    /**
      * Válida o tamanho mínimo e máximo de caracteres da string e os caracteres aceitos 
      *
      * @param string $string
@@ -13,7 +19,8 @@ class FormHelper
      * @param string $max_length
      * @return boolean
      */
-    public function validateStringLengthAndChars(string $string, string $min_length = '', string $max_length = ''): bool
+
+    public function validateStringLengthAndChars(string $string, string $min_length = '', string $max_length = '', array $allowed_chars = []): bool
     {
         $string = trim($string);
         if (is_string($string)) {
@@ -27,14 +34,21 @@ class FormHelper
         }
 
         if (!empty($allowed_chars)) {
-            foreach (str_split($string) as $char) {
-                if (!in_array($char, $allowed_chars)) {
+            foreach ($allowed_chars as $char) {
+                if (strpos($string, $char) === false) {
                     return false;
                 }
             }
         }
         return true;
     }
+
+    /**
+     * ####################
+     * ###   MÁSCARAS   ###
+     * ####################
+     */
+
     /**Adiciona mascara para para cpf e cnpj
      *  @param string | int $string
      *  @return string
@@ -64,5 +78,100 @@ class FormHelper
         } else {
             return $phone_number;
         }
+    }
+
+    /**
+     * ################################
+     * ###     ATAQUES/SEGURANÇA    ###
+     * ###############################
+     */
+
+    /**
+     * Função que previne ataques XSS. Deve ser usada ao exibir dados de formulários e bancos de dados.
+     *
+     * @param string $string A string a ser limpa e protegida contra ataques XSS.
+     * @return string A string limpa e segura contra ataques XSS.
+     */
+    public function basicCleanXSS(string $string)
+    {
+        $output = strip_tags($string);
+        //ENT_QUOTES é uma constante que significa que tanto as aspas duplas quanto as simples devem ser convertidas em entidades HTML
+        $output = htmlspecialchars($output, ENT_QUOTES | ENT_HTML5, 'UTF-8');
+        return $output;
+    }
+
+    /**
+     * Gera um token CSRF para formulários.
+     * O token CSRF deve ser validado no lado do servidor para proteção contra ataques CSRF.
+     *
+     * @return string O token CSRF gerado.
+     */
+    public static function gerarCsrf()
+    {
+        // Gera um código CSRF
+        $csrf_token = bin2hex(random_bytes(32));
+
+        // Armazena o código CSRF em uma variável de sessão
+        session_start();
+        $_SESSION['csrf_token'] = $csrf_token;
+        session_write_close();
+
+        return $csrf_token;
+    }
+
+    /**
+     * #######################
+     * ###      EMAIL      ###
+     * #######################
+     */
+    public function validateEmail($email, $allowed_chars = [])
+    {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return false;
+        }
+
+        if (!empty($allowed_chars)) {
+            foreach ($allowed_chars as $char) {
+                if (strpos($email, $char) === false) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Gera um formulário HTML com suporte a proteção CSRF.
+     *
+     * @param string $action O URL de destino do formulário.
+     * @param array $attributes Atributos adicionais para o formulário (opcional).
+     * @param string $content O conteúdo HTML do formulário (opcional).
+     * @param bool $csrfToken Define se deve ser gerado e incluído um token CSRF no formulário (padrão: false).
+     * @return string O código HTML do formulário gerado.
+     */
+    public static function form($action = '', $attributes = [], $content = '', $csrf_token = false)
+    {
+        $token = '';
+
+        if (empty($attributes)) {
+            $attributes = [
+                'name'              => '',
+                'method'            => 'POST',
+                'enctype'           => 'multipart/form-data',
+                'autocomplete'      => 'off',
+            ];
+        }
+        if ($csrf_token) {
+            $token = self::gerarCsrf();
+        }
+
+        $form = <<<FORM
+    <form name="{$attributes['name']}" action="$action" method="{$attributes['method']}" enctype="{$attributes['enctype']}" autocomplete="{$attributes['autocomplete']}">
+        <input type="hidden" name="csrf_token" value="$token">
+        $content
+    </form>
+    FORM;
+
+        return $form;
     }
 }
--- test/index.php
+++ test/index.php
@@ -3,20 +3,11 @@
 require __DIR__ . "../../vendor/autoload.php";
 
 use FormHelper\FormHelper;
-use Dotenv\Dotenv;
 
-$dotenv = Dotenv::createImmutable(__DIR__ . '/..');
-$dotenv->load();
 $form = new FormHelper;
-$form->validateStringLengthAndChars("teste teste@", 1, '', ['@']);
-print_r(
-    $form::maskPhoneNumber('4284356748')
-);
-// $data = $_POST;
-// $form->formNotEmpty($data);
 ?>
-<!-- <!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html>
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
@@ -26,18 +17,19 @@ print_r(
 </head>
 
 <body>
-    <form action="" method="post">
-        <label for="name">Name:</label>
-        <input type="text" name="name">
-        <br />
-        <label for="email">Email:</label>
-        <input type="email" name="email">
-        <br />
-        <label for="pass">Password:</label>
-        <input type="password" name="pass"><br />
-        <hr />
-        <input type="submit" value="Enviar">
-    </form>
+    <?php print_r($form->basicCleanXSS($_POST['teste']) ?? "") ?>
+    <?= $form::form(
+        '',
+        [],
+        <<<CONTENT
+    <h1>Teste</h1>
+    <input type="text" name="teste" id="" placeholder="Teste">
+    <input type="submit" name="enviar" placeholder="Enviar">
+    CONTENT,
+        true
+    ); ?>
+    <?php print_r($_SESSION) ?>
+
 </body>
 
-</html> -->
+</html>
